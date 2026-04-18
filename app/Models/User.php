c1fd8324ec47becf9a94b5ef0f69254af19fa3d0
Hoshinovaa <muhajirinrakan@gmail.com>
2026-04-18 17:22:00 +0700

    revisi dikit

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,33 +6,24 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-// WAJIB untuk Sanctum
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasApiTokens;
 
-    /**
-     * Kolom yang boleh diisi
-     */
+
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * Kolom yang disembunyikan
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Casting tipe data
-     */
     protected function casts(): array
     {
         return [
@@ -41,7 +32,6 @@ class User extends Authenticatable
         ];
     }
 
-    // 🔗 RELASI (opsional tapi bagus)
     public function subjects()
     {
         return $this->hasMany(Subject::class);
